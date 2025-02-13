e0f8219fa10b76c220ce0816895d387be1552052
John Blackwell <john.blackwell@treno.io>
2025-02-13 12:58:31 -0500

    script runs automaticlaly on worker nodes

--- modules/workers/main.tf
+++ modules/workers/main.tf
@@ -10,17 +10,23 @@ module "worker_nodes" {
   subnet_id              = var.private_subnet_id
   vpc_security_group_ids = [var.worker_sg_id]
 
-  user_data = <<EOF
-            #!/bin/bash
+  user_data = <<-EOF
+            #!/bin/bash -e
+
+            # Set environment variables
             echo "PLURAL_CONSOLE_URL=${var.plural_console_url}" | sudo tee -a /etc/environment
             echo "PLURAL_CONSOLE_TOKEN=${var.plural_console_token}" | sudo tee -a /etc/environment
+
             echo "export PLURAL_CONSOLE_URL=${var.plural_console_url}" | sudo tee -a /etc/profile.d/plural_env.sh
             echo "export PLURAL_CONSOLE_TOKEN=${var.plural_console_token}" | sudo tee -a /etc/profile.d/plural_env.sh
             sudo chmod +x /etc/profile.d/plural_env.sh
-            curl https://raw.githubusercontent.com/pluralsh/console-scale-testing/refs/heads/feat/vpc-and-bastion/workernode-scripts/plural-deployment-operator-install.sh
-            chmod +x plural-deployment-operator-install.sh
-            ./plural-deployment-operator-install.sh
+
+            # Download and execute the install script
+            curl -o /tmp/plural-deployment-operator-install.sh https://raw.githubusercontent.com/pluralsh/console-scale-testing/refs/heads/feat/jb-console-scale-testing/workernode-scripts/plural-deployment-operator-install.sh
+            chmod +x /tmp/plural-deployment-operator-install.sh
+            /tmp/plural-deployment-operator-install.sh
   EOF
+
 
   tags = {
     Name = var.worker_name
